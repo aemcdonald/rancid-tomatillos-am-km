e4fe53b029fe555455c23145b23ec5c2e4097375
Kristi Miller <kristi.ann.miller@gmail.com>
2020-10-07 14:19:41 -0600

    Add constructor and render methods to App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import MovieGrid from './MovieGrid.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {}
+    }
+  }
+  render() {
+    return (
+      <main className='App'>
+        <h1>Rancid Tomatillos</h1>
+      </main>
+    )
+  }
+}
+export default App
