1b93cc118701fa1c79fb00bf5b477aa1082621a6
Kristi Miller <kristi.ann.miller@gmail.com>
2020-10-08 17:05:10 -0600

    Add initial tests to App and MovieGrid test files

--- src/MovieGrid/MovieGrid.test.js
+++ src/MovieGrid/MovieGrid.test.js
@@ -3,3 +3,9 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import MovieGrid from './MovieGrid.js';
+
+describe('MovieGrid', () => {
+  it('Works', () => {
+    expect(true).toEqual(true)
+  })
+});
