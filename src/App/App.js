0708148db92bc7d463f5ab398845d322926d24a2
Kristi Miller <kristi.ann.miller@gmail.com>
2020-10-08 17:11:26 -0600

    Add react router and associated imports to App

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MovieGrid from '../MovieGrid/MovieGrid.js';
 import './App.css';
+
 
 
 class App extends Component {
