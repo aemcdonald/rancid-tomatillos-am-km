94e3a1189ed3854085bbaec2ffb2e42d6441a258
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-10-08 15:46:00 -0600

    Install testing libraries and add imports

---
+++ src/MovieCard/MovieCard.test.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import MovieCard from './MovieCard.js';
