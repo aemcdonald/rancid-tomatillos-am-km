b660fc1f71d37278d939a3c028baca1b4f45fd03
Kristi Miller <kristi.ann.miller@gmail.com>
2020-10-08 21:23:17 -0600

    Create MovieView component

--- src/MovieView/MovieView.js
+++ src/MovieView/MovieView.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ApiCalls from '../ApiCalls.js'
 import './MovieView.css';
 
-const MovieView = ({ title, avgRating, photo, releaseDate, history, movieId }) => {
-  return (
-    <section className='movieCard' onClick={() => {
-      history.push(`/${movieId}`)}}>
-      <h3>{title}</h3>
-      <h4>Average Rating: {avgRating}</h4>
-      <img className='poster' src={photo} alt={title} movie poster/>
-      <h5>Release Date: {releaseDate}</h5>
-    </section>
-  )
+class MovieView extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movie: {}
+    }
+  }
+  async componentDidMount() {
+    const movie = await ApiCalls.getSingleMovie(this.props.match.params.movieId)
+    this.setState({movie: movie.movie})
+  }
+  render() {
+    console.log(this.state);
+    return (
+      <section className='movieView'>
+        <h1>{this.state.movie.title}</h1>
+        <img className='poster' src={this.state.movie.backdrop_path} alt={this.state.movie.title} movie poster/>
+        <h4>{this.state.movie.overview}</h4>
+        <h5>Release Date: {this.state.movie.release_date}</h5>
+        <h6>Averge Rating: {this.state.movie.average_rating}</h6>
+      </section>
+    )
+  }
 }
+
 export default MovieView;
