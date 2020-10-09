bd7a7b845e8f56231420f6188e3b4669effd3e2b
Kristi Miller <kristi.ann.miller@gmail.com>
2020-10-08 20:02:12 -0600

    Clean up vestigial console.logs and hardcoded data

--- src/MovieGrid/MovieGrid.js
+++ src/MovieGrid/MovieGrid.js
@@ -6,32 +6,7 @@ class MovieGrid extends Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        {
-        "id": 694919,
-        "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
-        "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
-        "title": "Money Plane",
-        "average_rating": 9,
-        "release_date": "2020-09-29"
-        },
-        {
-        "id": 337401,
-        "poster_path": "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg",
-        "backdrop_path": "https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg",
-        "title": "Mulan",
-        "average_rating": 6,
-        "release_date": "2020-09-04"
-        },
-        {
-        "id": 718444,
-        "poster_path": "https://image.tmdb.org/t/p/original//uOw5JD8IlD546feZ6oxbIjvN66P.jpg",
-        "backdrop_path": "https://image.tmdb.org/t/p/original//x4UkhIQuHIJyeeOTdcbZ3t3gBSa.jpg",
-        "title": "Rogue",
-        "average_rating": 7,
-        "release_date": "2020-08-20"
-        }
-      ]
+      movies: []
     }
   }
 
@@ -43,6 +18,7 @@ class MovieGrid extends Component {
   }
 
   render() {
+    console.log(this.props.match);
     const movieCards = this.state.movies.map(movie => {
       return (
         <MovieCard
