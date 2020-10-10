f05bf4047b6d2fa8d09be941de9393adcee66b5f
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-10-09 18:39:17 -0600

    Create Login component & add imports

---
+++ src/Login/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ApiCalls from '../ApiCalls.js';
+import './Login.css';
+
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+}
+
+export default Login;
