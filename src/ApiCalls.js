61848ebac9457c62dd8a3f5789f275d51d625218
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-10-09 19:46:42 -0600

    Create postUserLogin method

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -4,6 +4,20 @@ const ApiCalls = {
     .then(response => response.json())
     .then(data => data)
     .catch(error => console.log(error.message))
+  },
+
+  postUserLogin(userLoginInfo) {
+    console.log(userLoginInfo)
+    return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userLoginInfo)
+    })
+    .then(response => response.json())
+    .then(data => console.log('success:', data))
+    .catch(err => console.log('failed', err.message))
   }
 }
 export default ApiCalls;
