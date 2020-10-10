dec93ed20bd54214f5891d0e23f9e6605791526b
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-10-09 19:08:03 -0600

    Create render method & getUserInput method

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -6,7 +6,39 @@ import './Login.css';
 class Login extends Component {
   constructor() {
     super();
-    this.state = {}
+    this.state = {
+      userEmail: '',
+      userPassword: ''
+      //name: 'Olivia', email: 'olivia@turing.io', password: 'pword'
+    }
+  }
+
+  getUserInput = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  render() {
+    return (
+      <form>
+        <input
+          type='text'
+          label='email input'
+          placeholder='Email'
+          name='userEmail'
+          value={this.state.userEmail.toLowerCase()}
+          onChange={this.getUserInput}
+        />
+        <input
+          type='password'
+          label='password input'
+          placeholder='Password'
+          name='userPassword'
+          value={this.state.userPassword}
+          onChange={this.getUserInput}
+        />
+        <button>Login</button>
+      </form>
+    )
   }
 }
 
