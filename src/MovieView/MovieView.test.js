aece4d66002c9e697a77b80ef9b0025cebf003b5
Kristi Miller <kristi.ann.miller@gmail.com>
2020-10-10 18:58:53 -0600

    Add initial test and imports

---
+++ src/MovieView/MovieView.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MovieView from './MovieView';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+//might need to npm install some stuff?
+
+describe('MovieView', () => {
+
+  it('should work', () => {
+    expect(true).toEqual(true);
+  })
+})
