52c18cd291e20084eb04c0a4993d46afc3a62a48
Kristi Miller <kristi.ann.miller@gmail.com>
2020-10-10 18:56:13 -0600

    Fix bug in movieCard alt text concatenation

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -7,7 +7,7 @@ const MovieCard = ({ title, avgRating, photo, releaseDate, history, movieId }) =
       history.push(`/${movieId}`)}}>
       <h3>{title}</h3>
       <h4>Average Rating: {avgRating}</h4>
-      <img className='poster' src={photo} alt={title} movie poster/>
+      <img className='poster' src={photo} alt={title + 'movie poster'}/>
       <h5>Release Date: {releaseDate}</h5>
     </section>
   )
