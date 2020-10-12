30d408b8f27e11ee3cd3f6dc7d24ee9485d51e5f
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-10-12 17:46:04 -0600

    Revise it block, remove pseudocode, organize assertions in test

--- src/Login/Login.test.js
+++ src/Login/Login.test.js
@@ -29,37 +29,22 @@ describe('Login', () => {
     expect(screen.getByText('Please type your email and password to sign in')).toBeInTheDocument()
   })
 
-  it('should submit the login information when clicked', async() => {
+  it('should welcome the user after submitting login info', async() => {
     ApiCalls.postUserLogin.mockResolvedValueOnce(
       {user: {id: 888, name: 'Olivia', email: 'olivia@turing.io'} }
     )
-    // const fakeSubmit = jest.fn();
-    // const fakeUser = {user: {id: 88, name: 'Olivia', email: 'olivia@turing.io'}}
- 
-    //render login
+
     render(<BrowserRouter><App /></BrowserRouter>)
     expect(screen.getByText('Login!')).toBeInTheDocument()
+
     userEvent.click(screen.getByText('Login!'))
     expect(screen.getByPlaceholderText('Email')).toBeInTheDocument()
-    userEvent.type(screen.getByPlaceholderText('Email'), 'test@turing.io')
+
+    userEvent.type(screen.getByPlaceholderText('Email'), 'olivia@turing.io')
     userEvent.type(screen.getByPlaceholderText('Password'), 'pword')
     userEvent.click(screen.getByText('Login'))
 
-
-
-    // render(
-    //   <Login
-    //     // {...fakeUser}
-    //     handleSubmit={fakeSubmit}
-    //   />
-    //)
     const welcomeMessage = await waitFor(() => screen.getByText('Welcome, Olivia!'))
-    //enter user info
-
-    //expect(fakeSubmit).toHaveBeenCalledTimes(1);
-    //click the button
-    //assert that submit login was called
-    //screen.debug()
     expect(welcomeMessage).toBeInTheDocument();
   })
   // it('should ')
