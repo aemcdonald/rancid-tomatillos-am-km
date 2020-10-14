7f1627564df9f191bc97b3c0827065eeb6655c32
Kristi Miller <kristi.ann.miller@gmail.com>
2020-10-13 20:06:33 -0600

    Create getUserRatings method

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -5,7 +5,7 @@ const ApiCalls = {
     .then(data => data)
     .catch(error => console.log(error.message))
   },
-  
+
   getSingleMovie(movieId) {
     return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${movieId}`)
     .then(response => response.json())
@@ -24,6 +24,14 @@ const ApiCalls = {
     .then(response => response.json())
     .then(data => data)
     .catch(err => console.log('failed', err.message))
-  }
+  },
+
+  getUserRatings(userId) {
+    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/users/${userId}/ratings`)
+    .then(response => response.json())
+    .then(data => data)
+    .catch(error => console.log(error.message))
+  },
+
 }
 export default ApiCalls;
