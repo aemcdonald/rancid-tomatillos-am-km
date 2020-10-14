2c70e006780603c2f02573a95ce3625532901696
Kristi Miller <kristi.ann.miller@gmail.com>
2020-10-13 20:27:32 -0600

    Add conditional to componentDidMount to add userRatings to state, clean up props in MovieCard render

--- src/MovieGrid/MovieGrid.js
+++ src/MovieGrid/MovieGrid.js
@@ -13,6 +13,13 @@ class MovieGrid extends Component {
 
   async componentDidMount() {
     const allMovies = await ApiCalls.getAllMovies()
+    if(this.props.currentUserId) {
+      let userRatings = await ApiCalls.getUserRatings(this.props.currentUserId)
+      userRatings.ratings.forEach((rating) => {
+        let foundMovie = allMovies.movies.find(movie => movie.id === rating.movie_id)
+        foundMovie.userRating = rating
+      })
+    }
     this.setState({movies: allMovies.movies})
   }
 
@@ -20,13 +27,8 @@ class MovieGrid extends Component {
     const movieCards = this.state.movies.map(movie => {
       return (
         <MovieCard
-          key={movie.id}
-          avgRating={movie.average_rating}
-          title={movie.title}
-          photo={movie.poster_path}
-          releaseDate={movie.release_date}
+          movie={movie}
           history={this.props.history}
-          movieId={movie.id}
         />
       )
     })
