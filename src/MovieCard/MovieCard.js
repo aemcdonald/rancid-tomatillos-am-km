4865bc702d739dcf98b6ee5ca4ed5acd956a8ba6
Kristi Miller <kristi.ann.miller@gmail.com>
2020-10-15 14:34:05 -0600

    Add conditional render for rating text

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import Rating from '../Rating/Rating.js';
 import './MovieCard.css';
 
 
+// <h4>{movie.userRating && `Your Rating: ${movie.userRating.rating}`}</h4>
+// <h4>{!movie.userRating && userId && `Add your rating`}</h4>
+// <h4>{!movie.userRating && !userId && `Sign in to add your rating`}</h4>
 
 
-const MovieCard = ({ movie, history }) => {
+const MovieCard = ({ movie, history, userId }) => {
   return (
     <section className='movieCard' onClick={() => {
       history.push(`/${movie.id}`)}}>
       <h1 className='cardTitle'>{movie.title}</h1>
       <h4>Average Rating: {parseFloat(movie.average_rating).toFixed(1)}</h4>
       <h4>{movie.userRating && `Your Rating: ${movie.userRating.rating}`}</h4>
+      <h4>{!movie.userRating && userId && `Click to add rating`}</h4>
+      <h4>{!movie.userRating && !userId && `Sign in to add your rating`}</h4>
       <img className='poster' src={movie.poster_path} alt={movie.title + ' movie poster'}/>
       <h5>Release Date: {movie.release_date}</h5>
     </section>
