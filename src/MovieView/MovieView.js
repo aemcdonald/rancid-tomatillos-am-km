7f83170a7c053b8a105657d8b9b6f746d53791d5
Kristi Miller <kristi.ann.miller@gmail.com>
2020-10-15 20:20:34 -0600

    Refactor Rating props

--- src/MovieView/MovieView.js
+++ src/MovieView/MovieView.js
@@ -12,13 +12,16 @@ class MovieView extends Component {
       hasRating: false
     }
   }
-  async componentDidMount() {
-    const singleMovieInfo = await ApiCalls.getSingleMovie(this.props.match.params.movieId)
+  getUserRating = async (singleMovieId) => {
     const userRatings = await ApiCalls.getUserRatings(this.props.currentUserId)
-    if(userRatings) {
-      const rating = userRatings.ratings.find(rating => rating.movie_id === singleMovieInfo.movie.id)
+    if(userRatings.ratings) {
+      const rating = userRatings.ratings.find(rating => rating.movie_id === singleMovieId)
       rating && this.setState({userRating: rating, hasRating: true})
     }
+  }
+  async componentDidMount() {
+    const singleMovieInfo = await ApiCalls.getSingleMovie(this.props.match.params.movieId)
+    this.getUserRating(singleMovieInfo.movie.id)
     this.setState({movie: singleMovieInfo.movie})
   }
   render() {
@@ -29,8 +32,8 @@ class MovieView extends Component {
           <h4 className='movieTagline'>{this.state.movie.tagline}</h4>
           <h4 className='movieOverview'>{this.state.movie.overview}</h4>
           <h5>Release Date: {this.state.movie.release_date}</h5>
-          {this.state.hasRating && <Rating userRating={this.state.userRating} rated={true}/>}
-          {!this.state.hasRating && <Rating rated={false} />}
+          {this.state.hasRating && <Rating updateRating={this.getUserRating} currentUser={this.props.currentUserId} userRating={this.state.userRating} rated={true} currentMovie={this.state.movie}/>}
+          {!this.state.hasRating && <Rating updateRating={this.getUserRating} currentUser={this.props.currentUserId} rated={false} currentMovie={this.state.movie}/>}
           <h6>Average Rating: {parseFloat(this.state.movie.average_rating).toFixed(1)}</h6>
         </section>
       </section>
