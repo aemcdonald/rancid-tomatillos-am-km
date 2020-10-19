584a89a1f9ee6b5e31dc184840cdcd443e91c60b
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-10-18 19:59:33 -0600

    Create test for displaying a user's rating

--- src/Rating/Rating.test.js
+++ src/Rating/Rating.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ApiCalls from '../ApiCalls.js';
-import MovieView from '../MovieView/MovieView';
+import Rating from './Rating.js';
+import MovieView from '../MovieView/MovieView.js';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
@@ -9,9 +10,40 @@ import { BrowserRouter } from 'react-router-dom';
 jest.mock('../ApiCalls');
 
 describe('Rating', () => {
-  it('works', () => {
-    expect(true).toEqual(true);
+  it('should display a user\'s rating if the movie is rated', async () => {
+    const mockProps = {match: {params: {movieId: 1234}}}
+    ApiCalls.getSingleMovie.mockResolvedValue(
+      { movie: {average_rating: 6, id: 1234, title: "Mulan"}}
+    )
+    ApiCalls.getUserRatings.mockResolvedValueOnce(
+      { ratings: [
+        {id: 2939, user_id: 88, movie_id: 5678, rating: 10},
+        {id: 2130, user_id: 88, movie_id: 1234, rating: 5}
+      ]}
+    )
+
+    render(
+      <BrowserRouter>
+        <MovieView {...mockProps} currentUserId={88}/>
+      </BrowserRouter>
+    );
+
+    const rating = await waitFor(() => screen.getByText('Your Rating: 5'))
+
+    expect(rating).toBeInTheDocument();
   })
-  //add a rating
-  //submit a rating
+
+  // display
+    //show 10 stars
+    //should fill the correct number of stars??
+
+  // logged in
+      //rated movie: Your rating 88832893298329
+      //not rated movie: Not yet rated
+      //add rating
+      //edit rating
+
+  //SAD PATH
+  //not logged in:
+    //we should test this on movieview
 })
