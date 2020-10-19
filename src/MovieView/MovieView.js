e2734235811270d2ec52b29ee5c42585447642b3
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-10-18 18:01:52 -0600

    Refactor MovieView to display refactored Rating component

--- src/MovieView/MovieView.js
+++ src/MovieView/MovieView.js
@@ -34,19 +34,37 @@ class MovieView extends Component {
     return (
       <section className='movieView' style={{backgroundImage: 'url(' + this.state.movie.backdrop_path + ')' }}>
         <section className='movieInfo'>
-          <h3 className='movieTitle'>{this.state.movie.title}</h3>
-          <h4 className='movieTagline'>{this.state.movie.tagline}</h4>
-          <h4 className='movieOverview'>{this.state.movie.overview}</h4>
-          <h5>Release Date: {this.state.movie.release_date}</h5>
-          <h4>{!this.props.currentUserId && 'Sign in to rate this movie!'}</h4>
-          {this.props.currentUserId && this.state.hasRating && <Rating updateRating={this.getUserRating} currentUser={this.props.currentUserId} userRating={this.state.userRating} rated={true} currentMovie={this.state.movie}/>}
-          {this.props.currentUserId && !this.state.hasRating && <Rating updateRating={this.getUserRating} currentUser={this.props.currentUserId} rated={false} currentMovie={this.state.movie}/>}
-          <h6>Average Rating: {parseFloat(this.state.movie.average_rating).toFixed(1)}</h6>
-        </section>
+        <h3 className='movieTitle'>{this.state.movie.title}</h3>
+        <h4 className='movieTagline'>{this.state.movie.tagline}</h4>
+        <h4 className='movieOverview'>{this.state.movie.overview}</h4>
+        <h5>Release Date: {this.state.movie.release_date}</h5>
+        <h4>{!this.props.currentUserId && 'Sign in to rate this movie!'}</h4>
+        {this.props.currentUserId && this.state.hasRating && <Rating userRating={this.state.userRating.rating}/>}
+        {this.props.currentUserId && !this.state.hasRating && <Rating />}
+        <h6>Average Rating: {parseFloat(this.state.movie.average_rating).toFixed(1)}</h6>
       </section>
+    </section>
     )
   }
 }
+
+//   render() {
+//     return (
+//       <section className='movieView' style={{backgroundImage: 'url(' + this.state.movie.backdrop_path + ')' }}>
+//         <section className='movieInfo'>
+//           <h3 className='movieTitle'>{this.state.movie.title}</h3>
+//           <h4 className='movieTagline'>{this.state.movie.tagline}</h4>
+//           <h4 className='movieOverview'>{this.state.movie.overview}</h4>
+//           <h5>Release Date: {this.state.movie.release_date}</h5>
+//           <h4>{!this.props.currentUserId && 'Sign in to rate this movie!'}</h4>
+//           {this.props.currentUserId && this.state.hasRating && <Rating updateRating={this.getUserRating} currentUser={this.props.currentUserId} userRating={this.state.userRating} rated={true} currentMovie={this.state.movie}/>}
+//           {this.props.currentUserId && !this.state.hasRating && <Rating updateRating={this.getUserRating} currentUser={this.props.currentUserId} rated={false} currentMovie={this.state.movie}/>}
+//           <h6>Average Rating: {parseFloat(this.state.movie.average_rating).toFixed(1)}</h6>
+//         </section>
+//       </section>
+//     )
+//   }
+// }
 
 export default MovieView;
 
