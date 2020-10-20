c7bb62acc523c4db78f1999031cc3d221a80f57e
Kristi Miller <kristi.ann.miller@gmail.com>
2020-10-20 17:48:03 -0600

    Add functionality to submit comment

---
+++ src/CommentForm/CommentForm.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ApiCalls from '../ApiCalls.js'
+import './CommentForm.css';
+
+class CommentForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comment: "",
+      author: this.props.userName
+    }
+  }
+  handleInput = (event) => {
+    this.setState({comment: event.target.value})
+  }
+  clearInput = () => {
+    this.setState({comment: ''})
+  }
+
+  render() {
+    return (
+      <form>
+        <input
+          name="comment"
+          type="text"
+          placeholder="Leave a comment about this movie"
+          value={this.state.comment}
+          onChange={this.handleInput}
+        />
+        <button type="button" onClick={() => {
+          this.props.addComment(this.state)
+          this.clearInput()}}>
+          SUBMIT</button>
+      </form>
+    )
+  }
+}
+export default CommentForm;
