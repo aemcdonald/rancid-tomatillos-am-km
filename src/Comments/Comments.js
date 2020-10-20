b8c31b94981161950f2be8009a912baf4ad0f7bb
Kristi Miller <kristi.ann.miller@gmail.com>
2020-10-20 17:48:30 -0600

    Add functionality to display new comment

--- src/Comments/Comments.js
+++ src/Comments/Comments.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ApiCalls from '../ApiCalls.js'
+import CommentForm from '../CommentForm/CommentForm.js'
 import './Comments.css';
 
 class Comments extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-
+      comments: []
     }
   }
+  async componentDidMount() {
+    const movieId = +this.props.movieId
+    const userComments = await ApiCalls.getAllComments(movieId)
+    if(Array.isArray(userComments.comments)) {
+      this.setState({comments: userComments.comments})
+    }
+  }
+  submitNewComment = async (commentInfo) => {
+    const newComment = await ApiCalls.postNewComment(commentInfo, this.props.movieId)
+    if(newComment.newComment.author && newComment.newComment.comment) {
+      this.setState({comments: [...this.state.comments, newComment.newComment]})
+    }
+  }
+
   render() {
+    const allComments = this.state.comments.map((comment, i) => {
+      return (
+        <section key={i}>
+          <h2>{comment.author}</h2>
+          <p>{comment.comment}</p>
+        </section>
+      )
+    })
     return (
-      <h4>COMMENTS GO HERE!</h4>
+      <section>
+        {this.props.userId.id && <CommentForm addComment={this.submitNewComment} userName={this.props.userId.name}/>}
+        <section className="allComments">
+          {this.state.comments.length ? allComments : 'No Comments Yet'}
+        </section>
+      </section>
     )
   }
 }
