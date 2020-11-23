29852a1fd91551997ed6457adce8b08fdb8479d4
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-23 15:41:24 -0700

    Add key to map

--- src/Comments/Comments.js
+++ src/Comments/Comments.js
@@ -4,7 +4,7 @@ import './Comments.css';
 const Comments = ({ allComments }) => {
     return(
         allComments.length > 0 ? allComments.map((comment, i) => {
-            return <h3>{comment.author}: {comment.comment}</h3>
+            return <h3 key={i}>{comment.author}: {comment.comment}</h3>
 
         }) : <h3>No Comments Yet!</h3>
     )
