7b444bce42f780de6cc0a9e1e2d4dece86b5f7da
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-23 13:54:15 -0700

    Create CommentForm component

---
+++ src/CommentForm/CommentForm.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './CommentForm.css';
+
+class CommentForm extends Component {
+    constructor() {
+        super();
+        this.state = {}
+    }
+
+    render() {
+        return(
+            <h1>CommentForm Goes Here</h1>
+        )
+    }
+}
+
+export default CommentForm;
