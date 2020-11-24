dd15c03de833c55d45ec8a0b4a1957876f3908d8
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-23 23:07:40 -0700

    Add imports, initial test

---
+++ src/CommentForm/CommentForm.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CommentForm from './CommentForm';  
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+
+describe('CommentForm', () => {
+    it('Should pass an initial test', () => {
+        expect(true).toBe(true)
+    })  
+})
