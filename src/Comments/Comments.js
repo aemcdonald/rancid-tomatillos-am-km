6b53ab42ea02631a8b047adbe2d03fe7ae3c715e
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-23 18:45:19 -0700

    Add PropTypes

--- src/Comments/Comments.js
+++ src/Comments/Comments.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Comments.css';
 
 const Comments = ({ allComments }) => {
@@ -11,3 +12,7 @@ const Comments = ({ allComments }) => {
 }
 
 export default Comments;
+
+Comments.propTypes = {
+    allComments: PropTypes.array
+}
