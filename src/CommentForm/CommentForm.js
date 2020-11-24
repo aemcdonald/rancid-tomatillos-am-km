3fd1e589bf09e8a14755cc235aaf6dc9e956e95b
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-23 20:43:45 -0700

    Create method to submit comment

--- src/CommentForm/CommentForm.js
+++ src/CommentForm/CommentForm.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
+import ApiCalls from '../ApiCalls.js'
 import './CommentForm.css';
 
 class CommentForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            author: this.props.currentUser.name,
-            comment: ''
+            comment: '',
+            author: this.props.currentUser.name
         }
     }
 
     handleChange = (event) => {
         this.setState({ comment: event.target.value })
+    }
+
+    submitComment = () => {
+        if (this.state.comment !== '') {
+            const newComment = this.state
+            this.props.addNewComment(newComment, this.props.movieId)
+        }
+        this.setState({ comment: '' })
     }
 
     render() {
