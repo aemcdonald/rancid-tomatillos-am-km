cfaf31e5ce0ee0a5c2a46359ab6c461cce39c24b
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-23 23:07:44 -0700

    Add imports, initial test

---
+++ src/Comments/Comments.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Comments from './Comments';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('Comments', () => {
+    it('Should pass an initial test', () => {
+        expect(true).toBe(true)
+    })
+})
